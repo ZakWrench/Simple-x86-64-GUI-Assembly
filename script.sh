7a3ce654f0b15262aa8cfd0357fbe2f5d2fdb749
ZakWrench <Zak.Wrench@gmail.com>
2023-07-05 17:22:22 +0100

    added advices, and a script to deactivate danger zone

--- script.sh
+++ script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Deactivate the red zone which is a 128 byte region at the bottom of the stack.
+nasm -f elf64 -g main.nasm && cc main.o -static -o main -mno-red-zone -nostdlib. 
+#Assemble main.nasm to create object file, then link it statically to create executable 'main'
 nasm -f elf64 -g main.nasm && ld main.o -static -o main
 
 ./main
